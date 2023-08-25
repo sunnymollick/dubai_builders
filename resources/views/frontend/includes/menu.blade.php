42ce713d99607353a50acd9438df8b0c55f2be66
Avishek Barua <avishekbarua02@gmail.com>
2023-08-25 13:52:48 +0600

    templating partially done

---
+++ resources/views/frontend/includes/menu.blade.php
@@ -0,0 +1,80 @@
+<div class="middle_bar">
+    <div class="container">
+        <div class="middle_bar_inner">
+            <div class="logo">
+                <a href="index.html" class="light_mode_logo"><img src="images/logo.svg" alt="logo"></a>
+                <a href="index.html" class="dark_mode_logo"><img src="images/logo_foot.svg" alt="logo"></a>
+            </div>
+
+            <div class="header_right_part">
+                <div class="mainnav">
+                    <ul class="main_menu">
+                        <li class="menu-item menu-item-has-children active"><a href="index.html">Home</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item active"><a href="index.html">Home One</a></li>
+                                <li class="menu-item"><a href="index-2.html">Home Two</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item menu-item-has-children"><a href="#">Services</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item"><a href="services.html">Service One</a></li>
+                                <li class="menu-item"><a href="services-2.html">Service Two</a></li>
+                                <li class="menu-item"><a href="service-details.html">Service Details</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item menu-item-has-children"><a href="#">Projects</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item"><a href="project.html">Projects One</a></li>
+                                <li class="menu-item"><a href="project-2.html">Peojects Two</a></li>
+                                <li class="menu-item"><a href="project-details.html">Peojects Details</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item menu-item-has-children"><a href="#">Pages</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item"><a href="about.html">About</a></li>
+                                <li class="menu-item"><a href="team.html">Team</a></li>
+                                <li class="menu-item"><a href="faq.html">FAQ</a></li>
+                                <li class="menu-item menu-item-has-children"><a href="#">Shop</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item"><a href="shop-1.html">Shop One</a></li>
+                                        <li class="menu-item"><a href="shop-2.html">Shop Two</a></li>
+                                        <li class="menu-item"><a href="shop-3.html">Shop Three</a></li>
+                                        <li class="menu-item"><a href="shop-4.html">Shop Four</a></li>
+                                        <li class="menu-item"><a href="product.html">Product Details</a></li>
+                                        <li class="menu-item"><a href="cart.html">Cart</a></li>
+                                        <li class="menu-item"><a href="checkout.html">Checkout</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-item menu-item-has-children"><a href="#">Blog</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item"><a href="blog-1.html">Blog One</a></li>
+                                <li class="menu-item"><a href="blog-2.html">Blog Two</a></li>
+                                <li class="menu-item"><a href="blog-details.html">Blog Details</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item"><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="phone">
+                    <i class="fa fa-phone" aria-hidden="true"></i>
+                    <div><span>Call Us Anytime</span><br><span class="phn_number">(+123)987.654.32</span></div>
+                </div>
+                <div class="header_search">
+                    <button type="submit" class="form-control-submit"><i class="ion-ios-search"></i></button>
+                </div>
+                <div class="open_search">
+                    <form class="search_form"
+                        action="https://wpthemebooster.com/demo/themeforest/html/builderrin/dark/search.php">
+                        <input type="text" name="search" class="keyword form-control" placeholder="Search..." />
+                        <button type="submit" class="form-control-submit"><i class="ion-ios-search"></i></button>
+                    </form>
+                </div>
+                <button class="ma5menu__toggle" type="button">
+                    <i class="ion-android-menu"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/frontend/includes/scripts.blade.php
@@ -0,0 +1,18 @@
+<!-- All JavaScript Files -->
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="plugins/swiper/swiper-bundle.min.js"></script>
+
+<script src="plugins/menu/ma5-menu.min.js"></script>
+<script src="plugins/owlcarousel/owl.carousel.min.js"></script>
+
+<!-- Select2 -->
+<script src="plugins/select2/js/select2.min.js"></script>
+
+<!-- Isotope -->
+<script src="plugins/isotope/isotope.pkgd.min.js"></script>
+<script src="plugins/isotope/imagesloaded.pkgd.min.js"></script>
+
+<script src="plugins/aos/aos.js"></script>
+<script src="js/funfacts.js"></script>
+<script src="js/custom.js"></script>
