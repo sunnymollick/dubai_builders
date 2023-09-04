fc7ea2f93a971709a976a0548ca9fcba72abb952
sunny <sunnymollick72@gmail.com>
2023-09-05 00:23:33 +0600

    client model location changed into backend

--- app/Http/Controllers/Backend/Admin/ClientController.php
+++ app/Http/Controllers/Backend/Admin/ClientController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Backend\Admin;
 use App\Http\Controllers\Controller;
-use App\Models\Client;
+use App\Models\Backend\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
