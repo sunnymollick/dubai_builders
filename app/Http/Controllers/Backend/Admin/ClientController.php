aecafaf4e650189f46b52c48712ee50726f9a492
sunny <sunnymollick72@gmail.com>
2023-09-10 00:39:49 +0600

    9/10/2023

--- app/Http/Controllers/Backend/Admin/ClientController.php
+++ app/Http/Controllers/Backend/Admin/ClientController.php
@@ -47,7 +47,6 @@ class ClientController extends Controller
      */
     public function create(Request $request)
     {
-        dd('create_project');
         if ($request->ajax()) {
             $view = View::make('backend.pages.clients.create')->render();
             return response()->json(['html' => $view]);
